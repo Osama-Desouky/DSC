04cd70a3f7566907befd016206b61a347fd40dfc
elomar1 <ahmedomarabdallah1@gmail.com>
2022-09-22 15:31:37 +0200

    adding home page and 3 pages

---
+++ assets/js/custom.js
@@ -0,0 +1,106 @@
+
+
+
+$( document ).ready(function() {
+
+    if($('.InHome').length ){
+        $('.homeBanner').addClass('homeClass')
+     }
+    $(".navbar-light .dmenu").hover(
+        function () {
+          $(this).find(".sm-menu").first().stop(true, true).slideDown(150);
+        },
+        function () {
+          $(this).find(".sm-menu").first().stop(true, true).slideUp(105);
+        }
+      );
+
+    let arrow = [
+        '<img src="../assets/images/home/arrowOwl2.svg" />',
+        '<img src="../assets/images/home/arrowOwl.svg" />',
+      ]
+      $('.owl-carousel-centerIn').owlCarousel({
+        loop:false,
+        margin:24,
+        nav:true,
+        rtl: true,
+        navText: arrow,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:4
+            }
+        }
+    })
+    // end owl center
+    $('.owl-carousel-lastNews').owlCarousel({
+        loop:false,
+        margin:30,
+        items:1,
+        nav:true,
+        rtl: true,
+        navText: arrow,
+    })
+    $('.owl-carousel-events').owlCarousel({
+        loop:false,
+        margin:30,
+        nav:true,
+        rtl: true,
+        navText: arrow,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1400:{
+                items:2
+            }
+        }
+    })
+    $('.owl-carousel-partners').owlCarousel({
+        loop:true,
+        margin:24,
+        nav:true,
+        rtl: true,
+        navText: arrow,
+        autoplay:true,
+        autoplayTimeout:1000,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            },
+            1400:{
+                items:6
+            }
+        }
+    })
+    // end ready
+    $('.owl-carousel-banner').owlCarousel({
+        loop:false,
+        margin:0,
+        nav:false,
+        rtl: true,
+        items:1,
+    })
+    // end ready
+
+
+    AOS.init({
+        duration: 1000,
+    });
+});
+
